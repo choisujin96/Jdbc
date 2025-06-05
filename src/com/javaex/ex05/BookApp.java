f0c57ac0e21b58f1a892f61998fdb2b8b77e32b8
choisujin96 <csjin9783@naver.com>
2025-06-05 17:45:17 +0900

    *과제 중*

--- src/com/javaex/ex05/BookApp.java
+++ src/com/javaex/ex05/BookApp.java
@@ -3,7 +3,23 @@ package com.javaex.ex05;
 public class BookApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		BookDAO bookDAO = new BookDAO();
+		
+		//책등록
+		bookDAO = bookInsert("태도에 관하여", "토스트", "2024.09.24",);
+		
+		//책수정
+		bookDAO.bookUpdate();
+		
+		//책삭제
+		bookDAO.bookDelete();
+		
+		//책리스트
+		bookDAO.bookSelect();
+		
+		bookDAO.bookSelectOne();
+		
 
 	}
 
